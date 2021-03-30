b99f4e92c9fe6831b277eab9a9c1409f7629ef1f
Steven Sarria <ssarr012@fiu.edu>
2021-03-30 01:17:49 -0400

    Home page
    Added introductory video to homepage + landing text.

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,10 +6,40 @@ import intromovie from "../images/intro logo movie.mp4"
 
 const Home = () => (
   <Layout>
-      <div className="Logo-Video">
-          <video controls autostart autoPlay src={intromovie} type="video/mp4" />
+    <>
+      <div>
+        <h3 class="flex text-white justify-center mt-4">Capturing life through a lens.</h3>
       </div>
+
+      <div className="Logo-Video" class="flex justify-center">
+          <video id="TriganVid" muted timeupdate poster autostart autoPlay src={intromovie} type="video/mp4"/>
+      </div>
+    </>
   </Layout>
 )
+
+// video.addEventListener("timeupdate", function(){
+//   if(this.currentTime >= 1 * 4) {
+//       this.pause();
+//   }
+// });
+
+// let intro=document.querySelectorAll('.video');
+// intro.forEach((video)=>{
+//  video.addEventListener('timeupdate', function(){
+//    if(this.currentTime === 1 * 6) {
+//      this.pause();
+//    }
+//  })
+// });
+
+window.onload=function(){
+  let video=document.getElementById("TriganVid");
+    video.addEventListener("timeupdate", function(){
+      if(this.currentTime >= 6){
+        video.pause();
+    }
+    });
+  }
 
 export default Home
