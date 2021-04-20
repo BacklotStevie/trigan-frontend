cde25774c5b48d3e576e72637a9a25fc6ffa8d01
Steven Sarria <ssarr012@fiu.edu>
2021-04-20 11:49:14 -0400

    Portfolio Page Design
    Began embedding videos onto the portfolio page from the client's Vimeo.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -12,6 +12,20 @@ const Portfolio = () => (
     <div class="flex items-center text-white justify-center pt-3">
             <h2 class="text-white">PORTFOLIO</h2>
     </div>
+    <div class="mt-5">
+      <div class="flex justify-around flex-wrap">
+      <h4 class="text-center text-white w-full pb-3">Travel Videos</h4>
+      <div class="w-full md:w-1/2 lg:w-1/3">
+        <iframe src="https://player.vimeo.com/video/537736986?embedparameter=value" width="100%" height="200" frameborder="0" allowfullscreen></iframe>
+      </div>
+      <div class="w-full md:w-1/2 lg:w-1/3">
+        <iframe src="https://player.vimeo.com/video/537735923?embedparameter=value" width="100%" height="200" frameborder="0" allowfullscreen></iframe>
+      </div>
+      <div class="w-full md:w-1/2 lg:w-1/3">
+        <iframe src="https://player.vimeo.com/video/537729922?embedparameter=value" width="100%" height="200" frameborder="0" allowfullscreen></iframe>
+      </div>
+      </div>
+    </div>
   </Layout>
 )
 
