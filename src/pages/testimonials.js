586f395b1ed434ee29c64094dffed2ea5c59df6c
Steven Sarria <ssarr012@fiu.edu>
2021-05-12 18:58:24 -0400

    Updated testimonial photos for client
    Updated testimonial photo to more accurately reflect a business partner of the client's.

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -5,7 +5,7 @@ import triganeye from "../images/trigan eye.png"
 import isho from "../images/Isho.jpeg"
 import mater from "../images/mater.png"
 import dbhoops from "../images/dbhoops.png"
-import didi from "../images/didi.jpeg"
+import didi from "../images/didi.png"
 
 const Testimonials = () => (
   <Layout>
@@ -18,7 +18,7 @@ const Testimonials = () => (
             <h2 class="text-white">TESTIMONIALS</h2>
         </div>
         <div class='flex justify-around flex-wrap pt-4'>
-            <img src={didi} alt="..." class="flex max-w-23 max-h-20 justify-self: center border-none"></img> 
+            <img src={didi} alt="..." class="flex max-w-10 max-h-10 justify-self: center border-none"></img> 
             <p class="text-white w-full md:w-2/3">"Working with Trigan Visuals was, and continues to be, an awesome experience! As a visual artist myself, I've had the pleasure of working alongside and with Trigan Visuals for my personal brand. Trigan always gives their clients full transparency while meeting all your expectations. Dedication, professionalism & creativity are just some of the many words I'd use to describe them! I would highly recommend Trigan Visuals to anyone looking for high-quality videography!" - <span class='text-red-700'>Desiree Prieto, <i>Artistic Desire</i></span></p>
         </div>
         <div class='flex justify-around flex-wrap pt-4'>
